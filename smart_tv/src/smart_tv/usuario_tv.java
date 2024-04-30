aa2d1963193ef93de5ff86f98c7907ba07f10d25
guilherme <guilhermesilvaimoveis@gmail.com>
2024-04-30 01:56:23 -0300

    projeto smartTv

--- smart_tv/src/smart_tv/usuario_tv.java
+++ smart_tv/src/smart_tv/usuario_tv.java
@@ -1,11 +1,34 @@
 package smart_tv;
 
 public class usuario_tv {
+		
+	public static void main(String[] args) throws Exception {
 
-	boolean ligada = false;
-	int canal = 1;
-	int volume = 25;
+	SmartTv smartTv = new SmartTv();
+	
+	smartTv.diminuirVolume();
+	smartTv.diminuirVolume();
+	smartTv.diminuirVolume();
+	smartTv.aumentarVolume();
+	smartTv.aumentarCanal();
+	smartTv.diminuirCanal();
+	
+	System.out.println("canal antes " + smartTv.canal);
+	
+	smartTv.mudarCanal(13);;
+	
+	System.out.println("canal atual " + smartTv.canal);
+	System.out.println(" volume atual " + smartTv.volume);
+	System.out.println("TV ligada ?" + smartTv.ligada);
+	System.out.println("canal atual " + smartTv.canal);
+	System.out.println("volume atual " + smartTv.volume);
+	
+	smartTv.ligar();
+	System.out.println(" novo Status TV ligada ?" + smartTv.ligada);
+	
+	smartTv.desligar();
+	System.out.println(" novo Status TV desligada " + smartTv.ligada);
 	
 		
-		
+		}	
 }
